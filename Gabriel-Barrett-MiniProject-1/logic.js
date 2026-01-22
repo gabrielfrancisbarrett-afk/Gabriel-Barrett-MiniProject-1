03bb1ca9eeb1558e052d4a6c497de317d301cd95
“gabrielfrancisbarrett-afk” <gabrielfrancisbarrett@gmail.com>
2026-01-22 16:54:57 +0000

    Hey will this is what we've got

--- Gabriel-Barrett-MiniProject-1/logic.js
+++ Gabriel-Barrett-MiniProject-1/logic.js
@@ -1,11 +1,15 @@
 let player = true
-
+let symbol
 let grid = document.querySelectorAll(".gridCells")
+
+let currentBoard = [
+  ["", "", ""],["", "", ""],["", "", ""]
+] 
+
 
 grid.forEach((box) => {
   box.addEventListener("click", () => {
     if (box.innerText !== "") return 
-    let symbol
     if (player) {
       box.innerText = "X"
       box.style.color = "red"
@@ -17,6 +21,10 @@ grid.forEach((box) => {
     console.log(column)
     currentBoard[row][column] = symbol
     console.log(currentBoard)
+    const winner = checkWinner(currentBoard)
+      if (winner) {
+        console.log(`${winner} wins!`)
+    } 
     } else {
       box.innerText = "O";
       box.style.color = "black"
@@ -29,34 +37,115 @@ grid.forEach((box) => {
     console.log(column)
     currentBoard[row][column] = symbol
     console.log(currentBoard)
+    const winner = checkWinner(currentBoard)
+      if (winner) {
+      console.log(`${winner} wins!`)
+    }
     }
   })
 })
 
-let currentBoard = [
-  ["", "", ""],["", "", ""],["", "", ""]
-]
+
 
 //actual board visualisation 
 // [0,1,2]
 // [3,4,5]
 // [6,7,8]
 
-const winSequence = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
-]
+// const winSequence = [
+//     [0, 1, 2],
+//     [0, 3, 6],
+//     [0, 4, 8],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [2, 4, 6],
+//     [3, 4, 5],
+//     [6, 7, 8]
+// ]
 
-let winner = () => {
-   if (i of winSequence) {
-    let valueOne = gridcells[i][0] = innerText
-    let valueTwo = gridcells[i][1] = innerText
-    let valueThree = gridcells[i][2] = innerText
-}}
+const winSequences = [
+  // rows
+  [[0, 0], [0, 1], [0, 2]],
+  [[1, 0], [1, 1], [1, 2]],
+  [[2, 0], [2, 1], [2, 2]],
+  // columns
+  [[0, 0], [1, 0], [2, 0]],
+  [[0, 1], [1, 1], [2, 1]],
+  [[0, 2], [1, 2], [2, 2]],
+  // diagonals
+  [[0, 0], [1, 1], [2, 2]],
+  [[0, 2], [1, 1], [2, 0]]
+];
+
+
+function getCell(board,row,column){
+  return board[row][column]
+}
+
+function checkLine(board, positions) {
+  const firstPosition = positions[0]
+  const secondPosition = positions[1]
+  const thirdPosition = positions[2]
+  const first = getCell(board, firstPosition[0],firstPosition[1])
+  const second = getCell(board, secondPosition[0],secondPosition[1])
+  const third = getCell(board, thirdPosition[0], thirdPosition[1])
+  if (first !== "" && first == second && first === third) {
+    return first
+  } return null
+}
+
+
+function checkWinner(board) {
+    for (let i=0;i < winSequences.length;i++) {
+      // const result = checkLine(board,line)
+      console.log(winSequences[i])
+      if (result !== null) {
+        return result
+      } 
+    } 
+}
+
+
+
+
+// function topRowWinner(board) {
+//   const first = board[0][0]
+//   const second = board[0][1]
+//   const third = board[0][2]
+//   if (first !== "" && first === second && first === third) {
+//     return first 
+//   }
+//   return null
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkWinner() {
+//   for(const sequence of winSequence){
+//     console.log("CHECKING")
+//     let value1 = sequence[0].innerText
+//     let value2 = sequence[1].innerText
+//     let value3 = sequence[2].innerText
+//     if (value1 !"" value2 && value3 === winSequence){
+//       console.log("Winner")
+//     }else{
+//         return 
+//       }
+
+//     }
+//   }
+
 
